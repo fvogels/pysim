41eaca18930f8d6e5cfff529608f1514079e3b12
Frederic Vogels <frederic.vogels@ucll.be>
2022-03-08 21:08:53 +0100

    Grid

---
+++ pysim/data/grid.py
@@ -0,0 +1,15 @@
+from typing import TypeVar, Generic, Callable
+from pysim.data.position import Position
+
+
+T = TypeVar('T')
+
+
+class Grid(Generic[T]):
+    __contents : list[list[T]]
+
+    def __init__(self, width, height, initializer : Callable[[Position], T]):
+        self.__contents = [[initializer(Position(x, y)) for x in range(width)] for y in range(height)]
+
+    def __getitem__(self, position : Position) -> T:
+        return self.__contents[position.y][position.x]
