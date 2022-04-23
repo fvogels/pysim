cf228244c1eb353f0dbd67c7e2bbb62b3398b590
Frederic Vogels <frederic.vogels@ucll.be>
2022-04-23 12:29:21 +0200

    extra linear float animation tests

--- tests/linear_float_animation_tests.py
+++ tests/linear_float_animation_tests.py
@@ -13,3 +13,10 @@ def test_start(start, stop, duration):
 def test_stop(start, stop, duration):
     animation = LinearFloatAnimation(start=start, stop=stop, duration=duration)
     assert animation[duration] == approx(stop)
+
+
+@mark.parametrize("start, stop, duration", [(start, stop, duration) for start in range(1, 10) for stop in range(1, 10) for duration in range(1, 10)])
+def test_middle(start, stop, duration):
+    animation = LinearFloatAnimation(start=start, stop=stop, duration=duration)
+    expected = (start + stop) / 2
+    assert animation[duration / 2] == approx(expected)
