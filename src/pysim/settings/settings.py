1716adfcd2193a0159fcf8f7c20d7b9a8ece9ca1
Frederic Vogels <frederic.vogels@ucll.be>
2022-04-23 13:48:13 +0200

    Settings

---
+++ src/pysim/settings/settings.py
@@ -0,0 +1,16 @@
+from typing import Any
+from abc import ABC, abstractmethod
+
+
+class Settings(ABC):
+    @abstractmethod
+    def __getitem__(self, key : str) -> Any:
+        ...
+
+    @abstractmethod
+    def __setitem__(self, key : str, value : Any) -> None:
+        ...
+
+    @abstractmethod
+    def __contains__(self, key : str) -> bool:
+        ...
