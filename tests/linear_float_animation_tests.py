d4fd3ceb495666a879b836961d416c284eb50ddd
Frederic Vogels <frederic.vogels@ucll.be>
2022-04-23 12:28:31 +0200

    linear float animation tests

---
+++ tests/linear_float_animation_tests.py
@@ -0,0 +1,15 @@
+from pysim.graphics.animations.float import LinearFloatAnimation
+from pytest import mark
+from pytest import approx
+
+
+@mark.parametrize("start, stop, duration", [(start, stop, duration) for start in range(1, 10) for stop in range(1, 10) for duration in range(1, 10)])
+def test_start(start, stop, duration):
+    animation = LinearFloatAnimation(start=start, stop=stop, duration=duration)
+    assert animation[0] == start
+
+
+@mark.parametrize("start, stop, duration", [(start, stop, duration) for start in range(1, 10) for stop in range(1, 10) for duration in range(1, 10)])
+def test_stop(start, stop, duration):
+    animation = LinearFloatAnimation(start=start, stop=stop, duration=duration)
+    assert animation[duration] == approx(stop)
