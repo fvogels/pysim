c50f4039c852907f8071c3bbb05bfa6684d3ac5e
Frederic Vogels <frederic.vogels@ucll.be>
2022-04-26 14:54:36 +0200

    environment uses actions

--- src/pysim/environments/simple.py
+++ src/pysim/environments/simple.py
@@ -1,16 +1,24 @@
 from pysim.actors.actor import Channel, export
-
+import pysim.simulation.actions as actions
 
 class SimpleEnvironment:
-    __channel : Channel[str]
+    __channel : Channel[actions.Action]
 
-    def __init__(self, channel : Channel[str]):
+    def __init__(self, channel : Channel[actions.Action]):
         self.__channel = channel
 
     @export
     def forward(self) -> None:
-        self.__channel.send('forward')
+        self.__channel.send(actions.ForwardAction())
+
+    @export
+    def backward(self) -> None:
+        self.__channel.send(actions.BackwardAction())
 
     @export
     def turn_left(self) -> None:
-        self.__channel.send('turn-left')
+        self.__channel.send(actions.TurnLeftAction())
+
+    @export
+    def turn_right(self) -> None:
+        self.__channel.send(actions.TurnRightAction())
